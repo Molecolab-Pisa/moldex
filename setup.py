740d8af3713bb9efc236b5a1e306333f0a81bbbb
Amanda Arcidiacono <amy.arci@gmail.com>
2023-03-28 18:39:54 +0200

    Push project to GitLab

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="moldex",
+    version="0.0.0",
+    url="https://molimen1.dcci.unipi.it/molecolab/moldex.git",
+    author="Amanda Arcidiacono",
+    author_email="amy.arci@gmail.com",
+    description=open("README.md").read(),
+    packages=["moldex"],
+)
