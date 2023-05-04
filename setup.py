f3d202186c1bb69be504a76f296f0d644a4eb849
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-05-04 17:00:53 +0200

    added me and patty as authors

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="moldex",
-    version="0.0.0",
+    version="0.0.1",
     url="https://molimen1.dcci.unipi.it/molecolab/moldex.git",
-    author="Amanda Arcidiacono",
+    author="Amanda Arcidiacono, Patrizia Mazzeo, Edoardo Cignoni",
     author_email="amy.arci@gmail.com",
     description=open("README.md").read(),
     packages=["moldex"],
