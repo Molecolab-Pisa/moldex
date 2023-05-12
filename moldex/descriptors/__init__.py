a7754d904556c21ae19cb3cc029378077e6e4a45
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-05-12 17:35:37 +0200

    added example for angle_matrix

--- moldex/descriptors/__init__.py
+++ moldex/descriptors/__init__.py
@@ -1,5 +1,4 @@
-from .bond_matrix import bond_matrix, batched_bond_matrix
 from .angle_matrix import angle_matrix, batched_angle_matrix
-from .inverse_distance import inverse_distance_matrix
+from .bond_matrix import bond_matrix, batched_bond_matrix
 from .coulomb_matrix import coulomb_matrix
 from .inverse_distance import inverse_distance_matrix
