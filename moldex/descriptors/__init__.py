ed95d1da96e9e49a561c351233437cce8ee9bac8
Patrizia Mazzeo <p.mazzeo1@studenti.unipi.it>
2023-07-21 14:18:12 +0200

    Added CoulombMatrix class

--- moldex/descriptors/__init__.py
+++ moldex/descriptors/__init__.py
@@ -1,6 +1,6 @@
 from .angle_matrix import angle_matrix, batched_angle_matrix
 from .bond_matrix import bond_matrix, batched_bond_matrix
-from .coulomb_matrix import coulomb_matrix
+from .coulomb_matrix import coulomb_matrix, CoulombMatrix
 from .dihedral_matrix import dihe_matrix, batched_dihe_matrix
 from .inverse_distance import inverse_distance_matrix
 from .electrostatic_potential import electrostatic_potential, MMElectrostaticPotential
